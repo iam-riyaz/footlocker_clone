04d1abc46abd03eaa6786e5b12ef4638240de3eb
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-07-17 23:59:55 +0530

    next phase

--- footlocker/src/App.jsx
+++ footlocker/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Footer } from './Components/Footer'
 import { Navbar } from './Components/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   )
 }
